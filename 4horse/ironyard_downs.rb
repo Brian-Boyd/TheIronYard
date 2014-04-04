bf30fc60b199fdd4993cfe188dd2b419a5832986
Brian Boyd <brianboyd@Brians-MacBook-Air.local>
2014-04-04 17:52:59 -0400

    revised code

--- 4horse/ironyard_downs.rb
+++ 4horse/ironyard_downs.rb
@@ -5,101 +5,43 @@ start_screen
 
 @horses = []
 
-horse1 = Horse.new("My Horse")
-@horses.push(horse1)
+horse1 = Horse.new("IronYard Horse")
+  @horses.push(horse1)
 horse2 = Horse.new("Seabiscuit")
-@horses.push(horse2)
+  @horses.push(horse2)
 horse3 = Horse.new("Secretariat")
-@horses.push(horse3)
+  @horses.push(horse3)
 horse4 = Horse.new("Gravedigger")
-@horses.push(horse4)
+  @horses.push(horse4)
 
 clear
+puts "Starting lineup"
 display_track
 
+puts "Press enter to start race!"
+
+# def horse_position
+#   if @horses[0].position.to_i > 19
+#     puts "#{@horses[0].name} is the winner"
+#   elsif @horses[1].position.to_i > 19
+#     puts "#{@horses[1].name} is the winner"
+#   elsif @horses[2].position.to_i > 19
+#     puts "#{@horses[2].name} is the winner"
+#   elsif @horses[3].position.to_i > 19
+#     puts "#{@horses[3].name} is the winner"
+#   end
+# end
+
 $i = 0
-$num = 20 # @horses.value.position
+$num = 20
+# $num = horse_position.to_i
 
 while $i < $num  do
    turn
-   $i +=1
+   $i += 1
 end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# raceon = true
-
-# while raceon == true
-#   a = 20
-# puts "Enter to continue"
-# play = gets.chomp
-# system "clear"
-#   puts "----------------------------------------------------"
-
-#   (1..20).each do |number|
-#     print " *"
-#     if number == 20
-#       raceon = false
-#     end
-#   end
-# end
-#   puts "\n----------------------------------------------------"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# test_1 = true
-
-# while test_1 == true
-#   a = [1,2,3,4]    # rand(1..4)
-#   puts "----------------------------------------------------"
-
-#   (1..20).each do |number|
-#     print " *"
-#     if number == 20
-#       test_1 = false
-#     end
-#   end
-# end
-#   puts "\n----------------------------------------------------"
-
-
-
-
-
-# $i = 0
-# $num = 20
-
-# while $i < $num  do
-#    print("*" )
-#    $i +=1
-# end
+# How do I comment line 36 and use line 37 instead?
+# Lines 23-33 seem logical, but it doesn't work as the race does not even run
 
--- 4horse/track.rb
+++ 4horse/track.rb
@@ -1,7 +1,7 @@
 # Welcome Screen
 def start_screen
   system "clear"
-  print "\n\rWelcome to Iron Yard Downs"; sleep 3
+  print "\n\rWelcome to Iron Yard Downs"; sleep 2
   system "clear"
   print "\n\rOn your mark, "; sleep 1
   print "get set, "; sleep 1
@@ -19,34 +19,35 @@ def display_track
   puts "--------------------------------------------------\n\n"
 end
 
-# def turn
-#   @horses.each do |value|
-#     value.move_forward
-#   end
-#   play = gets.chomp
-#   system "clear"
-#   display_track
-# end
-
 def turn
-  if cheat == fast
+  cheat = gets.chomp.to_i
+  if cheat == 1
     @horses[0].position += 2
     @horses.each do |value|
-    value.move_forward
-  end
-  elsif cheat == faster
+      value.move_forward
+    end
+  elsif cheat == 2
     @horses[0].position += 3
     @horses.each do |value|
-    value.move_forward
-  end
-  elsif cheat == fastest
+      value.move_forward
+    end
+  elsif cheat == 3
     @horses[0].position += 4
     @horses.each do |value|
-    value.move_forward
+      value.move_forward
+    end
+  elsif cheat == 0
+    @horses.each do |value|
+      value.move_forward
+    end
   end
-  play = gets.chomp
   system "clear"
   display_track
+  cheat_commands
+end
+
+def cheat_commands
+  puts "Enter... 1 for fast | 2 for faster | 3 for fastest"
 end
 
 def clear
