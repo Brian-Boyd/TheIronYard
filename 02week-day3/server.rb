04bc2477c3d20b0b5e1b4fbb3d42ca0bf6bd1c59
Brian Boyd <brian@cubicalweb.com>
2014-04-12 10:09:49 -0400

    revised code

--- 02week-day3/server.rb
+++ 02week-day3/server.rb
@@ -1,38 +1,30 @@
 require 'sinatra'
 require 'faker'
 
-@person_name            = Faker::Name.name
-@person_email           = Faker::Internet.email
-@person_company         = Faker::Company.name
-@person_company_slogan  = Faker::Company.bs
-@person_phone           = Faker::PhoneNumber.phone_number
-@person_address         = Faker::Address.street_address
-@person_city            = Faker::Address.city
-@person_state           = Faker::Address.state_abbr
-@person_zip             = Faker::Address.zip_code
-
-
-
-def paragraph(sentence_count = 3, supplemental = false, random_sentences_to_add = 3)
-  sentences(resolve(sentence_count) + rand(random_sentences_to_add.to_i).to_i, supplemental).join(' ')
-end
-
 # Home page 
 get '/' do
-  @root = "This is the home page!"
+  @para = []
+  6.times do
+    @para.push(
+    {
+      para: Faker::Lorem.paragraphs(4)
+    })
+  end
   erb :home, layout: :index
 end
 
 # About page (about.erb)
 get '/about' do
-  @team = [
-    {name: Faker::Name.name, city: Faker::Address.city, state: Faker::Address.state},
-    {name: Faker::Name.name, city: Faker::Address.city, state: Faker::Address.state},
-    {name: Faker::Name.name, city: Faker::Address.city, state: Faker::Address.state},
-    {name: Faker::Name.name, city: Faker::Address.city, state: Faker::Address.state},
-    {name: Faker::Name.name, city: Faker::Address.city, state: Faker::Address.state},
-    {name: Faker::Name.name, city: Faker::Address.city, state: Faker::Address.state}
-  ]
+
+  @team = []
+  10.times do
+    @team.push(
+    { 
+      name:   Faker::Name.name, 
+      city:   Faker::Address.city, 
+      state:  Faker::Address.state
+    })
+  end
   erb :about, layout: :index
 end
 
@@ -46,20 +38,7 @@ get '/services' do
   erb :services, layout: :index
 end
 
-
-
-
-
-# Navigation, but do not want this as a page
-# get '/nav' do
-#   @nav = [
-#     {title: "Home", link: "/"},
-#     {title: "About", link: "/about"},
-#     {title: "Product", link: "/product"}
-#   ]
-#   erb :nav, layout: :index
-# end
-
-# <% @nav.each do |link| %>
-# <a href="<%= link[:link] %>"><%= link[:title] %></a>
-# <% end %>
+get '/sidebar_partial1'  do
+  @para = Faker::Lorem.sentences(3)
+  erb :sidebar, layout: :index
+end
